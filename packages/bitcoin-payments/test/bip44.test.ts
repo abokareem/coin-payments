33bcff188e98317d9e0bdfe0bdecd9a524c5b616
Dylan Seago <dylan.seago@gmail.com>
2020-01-07 16:20:56 -0500

    Move bip44 test fixtures to new file

--- packages/bitcoin-payments/test/bip44.test.ts
+++ packages/bitcoin-payments/test/bip44.test.ts
@@ -8,26 +8,12 @@ import {
 } from '../src/bip44'
 import { NETWORK_MAINNET, AddressType } from '../src'
 import { fromBase58 } from 'bip32'
+import {
+  DERIVED_XPRV, DERIVATION_PATH, ROOT_XPRV, NETWORK, PARTIALLY_DERIVED_XPRV, ADDRESS_LEGACY,
+  ADDRESS_SEGWIT_P2SH, ADDRESS_SEGWIT_NATIVE, PRIVATE_KEY, DERIVED_XPUB,
+} from './fixtures'
 
-const NETWORK = NETWORK_MAINNET
-const DERIVATION_PATH = "m/44'/0'/0'"
-const ROOT_XPRV = 'xprv9s21ZrQH143K3z2wCDRa3rHg9CHKedM1GvbJzGeZB14tsFdiDtpY6T96c1wWr9rwWhU5C8zcEWFbBVa4T3A8bhGSESDG8Kx1SSPfM2rrjxk'
-
-/** ROOT_XPRV derived to m/44' */
-const PARTIALLY_DERIVED_XPRV = 'xprv9txb3M6QPHVuD3ntLYvHdGbtBreGU93oQN2oHW2cBh2HUhUxm9HtDznowwgiZwKu7cXDwmrXSiRv593emimLhNUe43o7yCzN5euT92cfDpk'
-
-/** ROOT_XPRV derived to DERIVATION_PATH */
-const DERIVED_XPRV = 'xprv9yhZbBnMpH6yYJYEgifXpDwmJoyEbNSDjQKagEdnWtptM3yX2eNebybYgzGDxkcJHuLKhJeiicnvkPALkXifBqCA2Se9y48hNGgPM7RSxqW'
-
-/** XPUB of DERIVED_XPRV */
-const DERIVED_XPUB = 'xpub6CguzhKFeefGknchnkCYBMtVrqoizqA56dFBUd3Q5EMsDrJfaBgu9mv2YJTd3BANQhrcqmMKKPtYBETWF6SYD9G1Jubjze6VkZsX7DchtAi'
-
-const BASE_NODE = fromBase58(DERIVED_XPRV)
-
-const ADDRESS_LEGACY = '1J6t2jXst1Li2W9haExrThEBJeJaKpDCqY'
-const ADDRESS_SEGWIT_P2SH = '3ENA2DiDQLxuTLmsaaD4sSNyCFgRfB8Ck1'
-const ADDRESS_SEGWIT_NATIVE = 'bc1qhwtdp4q08553qkmlher7qe98n68mzn6tvxarz7'
-const PRIVATE_KEY = 'L1yJZhQJ3RNW1hTD53NeovU8Wj4iFnT6v47ZXPrsCY8ikvqH4xzg'
+export const BASE_NODE = fromBase58(DERIVED_XPRV)
 
 describe('bip44', () => {
   describe('splitDerivationPath', () => {
---
+++ packages/bitcoin-payments/test/fixtures/index.ts
@@ -0,0 +1,19 @@
+import { NETWORK_MAINNET } from '../../src'
+
+export const NETWORK = NETWORK_MAINNET
+export const DERIVATION_PATH = "m/44'/0'/0'"
+export const ROOT_XPRV = 'xprv9s21ZrQH143K3z2wCDRa3rHg9CHKedM1GvbJzGeZB14tsFdiDtpY6T96c1wWr9rwWhU5C8zcEWFbBVa4T3A8bhGSESDG8Kx1SSPfM2rrjxk'
+
+/** ROOT_XPRV derived to m/44' */
+export const PARTIALLY_DERIVED_XPRV = 'xprv9txb3M6QPHVuD3ntLYvHdGbtBreGU93oQN2oHW2cBh2HUhUxm9HtDznowwgiZwKu7cXDwmrXSiRv593emimLhNUe43o7yCzN5euT92cfDpk'
+
+/** ROOT_XPRV derived to DERIVATION_PATH */
+export const DERIVED_XPRV = 'xprv9yhZbBnMpH6yYJYEgifXpDwmJoyEbNSDjQKagEdnWtptM3yX2eNebybYgzGDxkcJHuLKhJeiicnvkPALkXifBqCA2Se9y48hNGgPM7RSxqW'
+
+/** XPUB of DERIVED_XPRV */
+export const DERIVED_XPUB = 'xpub6CguzhKFeefGknchnkCYBMtVrqoizqA56dFBUd3Q5EMsDrJfaBgu9mv2YJTd3BANQhrcqmMKKPtYBETWF6SYD9G1Jubjze6VkZsX7DchtAi'
+
+export const ADDRESS_LEGACY = '1J6t2jXst1Li2W9haExrThEBJeJaKpDCqY'
+export const ADDRESS_SEGWIT_P2SH = '3ENA2DiDQLxuTLmsaaD4sSNyCFgRfB8Ck1'
+export const ADDRESS_SEGWIT_NATIVE = 'bc1qhwtdp4q08553qkmlher7qe98n68mzn6tvxarz7'
+export const PRIVATE_KEY = 'L1yJZhQJ3RNW1hTD53NeovU8Wj4iFnT6v47ZXPrsCY8ikvqH4xzg'
